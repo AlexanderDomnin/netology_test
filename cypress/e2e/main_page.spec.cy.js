7a8a5ca75ebfc45871394bfe7e426989220e8954
aledomnin <aledomnin@ozon.ru>
2022-12-15 16:19:56 +0300

    add tests

--- cypress/e2e/main_page.spec.cy.js
+++ cypress/e2e/main_page.spec.cy.js
@@ -1,3 +1,38 @@
-describe('chart card view', () => {
+import {header, imgName, movie} from "../support/consts";
 
+describe('Test main page', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+    it('Test main page elements', () => {
+        const currentDate = new Date().setHours(0,0,0).toString().slice(0,-3)
+        cy.get('.page-header__title').should('have.text', header.headerTitle);
+        cy.get('.page-nav__day_today')
+            .should('be.visible')
+            .should('have.attr', 'data-time-stamp', `${currentDate}`)
+        cy.get('.page-nav__day_weekend').should('be.visible')
+        cy.get('.movie').should('be.visible')
+        cy.get('.movie__poster-image')
+            .should('be.visible')
+            .should('have.attr', 'src', imgName.imgLogan)
+        cy.get('.movie__description')
+            .first()
+            .should('be.visible')
+        cy.get('.movie__title')
+            .first()
+            .should('have.text',movie.logan)
+        cy.get('.movie__synopsis')
+            .first()
+            .should('have.text',movie.wolverine)
+        cy.get('.movie__data')
+            .first()
+            .should('contain.text','90 мин.')
+        cy.get('.movie-seances__time.acceptin-button-disabled')
+            .first()
+            .should('be.visible')
+        cy.get('.movie-seances__time')
+            .first()
+            .should('be.visible')
+            .should('not.have.attr','class','acceptin-button-disabled' )
+    })
 })
---
+++ cypress/support/consts.js
@@ -0,0 +1,12 @@
+export const imgName = {
+    imgLogan: '../img/posters/93.png',
+}
+
+export const header = {
+    headerTitle: 'Идёмвкино',
+}
+
+export const movie = {
+    logan: 'Логан',
+    wolverine: 'Россомаха'
+}
